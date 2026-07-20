0bace6d44c95eab16f62b58f56433d032472d9c1
Roman <romansidorcuk1@gmail.com>
2026-07-20 22:30:01 +0300

    Add implementation of JpaSpecificationExecutor to repostirory interfaces

--- userservice/src/main/java/com/innowise/userservice/repository/PaymentCardRepository.java
+++ userservice/src/main/java/com/innowise/userservice/repository/PaymentCardRepository.java
@@ -2,12 +2,13 @@ package com.innowise.userservice.repository;
 
 import com.innowise.userservice.entity.PaymentCard;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
-public interface PaymentCardRepository extends JpaRepository<PaymentCard, Long> {
+public interface PaymentCardRepository extends JpaRepository<PaymentCard, Long>, JpaSpecificationExecutor<PaymentCard> {
 
     @Modifying
     @Query("UPDATE PaymentCard p SET p.active = :active WHERE p.id = :id")
