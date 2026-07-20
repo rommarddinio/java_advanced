434a2b938d873641b1bbea77f0a6bde238e1b775
Roman <romansidorcuk1@gmail.com>
2026-07-20 13:26:40 +0300

    Add specific methods in repository interfaces

--- userservice/src/main/java/com/innowise/userservice/repository/PaymentCardRepository.java
+++ userservice/src/main/java/com/innowise/userservice/repository/PaymentCardRepository.java
@@ -2,6 +2,21 @@ package com.innowise.userservice.repository;
 
 import com.innowise.userservice.entity.PaymentCard;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface PaymentCardRepository extends JpaRepository<PaymentCard, Long> {
+
+    @Modifying
+    @Query("UPDATE PaymentCard SET PaymentCard.active = :active WHERE PaymentCard.id = :id")
+    void setActive(Long id, Boolean active);
+
+    List<PaymentCard> findByUserId(Long id);
+
+    @Modifying
+    @Query ("UPDATE PaymentCard p SET p.active = false WHERE p.user.id = :id")
+    void deactivateByUserId(Long id);
+
 }
--- userservice/src/main/java/com/innowise/userservice/repository/UserRepository.java
+++ userservice/src/main/java/com/innowise/userservice/repository/UserRepository.java
@@ -2,6 +2,17 @@ package com.innowise.userservice.repository;
 
 import com.innowise.userservice.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    @Modifying
+    @Query("UPDATE User SET User.active = :active WHERE User.id = :id")
+    void setActive(Long id, Boolean active);
+
+    Optional<User> findByEmail(String email);
+
 }
