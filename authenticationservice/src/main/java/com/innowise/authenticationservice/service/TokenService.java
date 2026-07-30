37f08d43afe4d8e36ac53de378cd5bbed6511a6b
Roman <romansidorcuk1@gmail.com>
2026-07-31 01:48:53 +0300

    Add token service interface and its implementation

---
+++ authenticationservice/src/main/java/com/innowise/authenticationservice/service/TokenService.java
@@ -0,0 +1,19 @@
+package com.innowise.authenticationservice.service;
+
+import io.jsonwebtoken.Claims;
+
+public interface TokenService {
+
+    String generateAccessToken(Long userId, String role);
+
+    String generateRefreshToken(Long userId, String role);
+
+    Claims getClaimsFromToken(String token);
+
+    Long getUserId(String token);
+
+    String getRole(String token);
+
+    String getTokenType(String token);
+
+}
