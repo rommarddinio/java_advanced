9bc9de8a91544984eaedc70c979a6dbd04466be6
Roman <romansidorcuk1@gmail.com>
2026-07-30 12:12:15 +0300

    Add CredentialService and its implementation

---
+++ authenticationservice/src/main/java/com/innowise/authenticationservice/service/impl/CredentialServiceImpl.java
@@ -0,0 +1,24 @@
+package com.innowise.authenticationservice.service.impl;
+
+import com.innowise.authenticationservice.entity.Credentials;
+import com.innowise.authenticationservice.repository.CredentialsRepository;
+import com.innowise.authenticationservice.service.CredentialsService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class CredentialServiceImpl implements CredentialsService {
+
+    private CredentialsRepository credentialsRepository;
+
+    @Override
+    public void saveCredentials(Credentials credentials) {
+        Credentials savedCredentials = credentialsRepository.save(credentials);
+
+        log.info("Successfully saved credentials with id = {}", savedCredentials.getId());
+    }
+
+}
