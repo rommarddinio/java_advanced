817ea9ecdcdf1f6dcde1cba7e162d9d97375d7e6
Roman <romansidorcuk1@gmail.com>
2026-07-30 12:13:24 +0300

    Add findByLogin method in CredentialsRepository

--- authenticationservice/src/main/java/com/innowise/authenticationservice/repository/CredentialsRepository.java
+++ authenticationservice/src/main/java/com/innowise/authenticationservice/repository/CredentialsRepository.java
@@ -4,6 +4,11 @@ import com.innowise.authenticationservice.entity.Credentials;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CredentialsRepository extends JpaRepository<Credentials, Long> {
+
+    Optional<Credentials> findByLogin(String login);
+    
 }
