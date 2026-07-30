6c4261ec829eb2fb2b3686fb8ed7e139c3fd1bc0
Roman <romansidorcuk1@gmail.com>
2026-07-31 02:07:08 +0300

    Add necessary annotations to enable web and method security

--- authenticationservice/src/main/java/com/innowise/authenticationservice/configuration/SecurityConfig.java
+++ authenticationservice/src/main/java/com/innowise/authenticationservice/configuration/SecurityConfig.java
@@ -8,7 +8,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -18,6 +20,8 @@ import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
 @Configuration
 @RequiredArgsConstructor
+@EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
 
     private final UserDetailsService userDetailsService;
