ceeff56a1695f05637ac22573bbc2aeb800cb7fb
Roman <romansidorcuk1@gmail.com>
2026-07-31 01:09:13 +0300

    Add custom user details

---
+++ authenticationservice/src/main/java/com/innowise/authenticationservice/details/UserDetailsImpl.java
@@ -0,0 +1,33 @@
+package com.innowise.authenticationservice.details;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+public class UserDetailsImpl implements UserDetails {
+
+    private Long userId;
+
+    private String username;
+
+    private String password;
+
+    private String role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role));
+    }
+
+}
