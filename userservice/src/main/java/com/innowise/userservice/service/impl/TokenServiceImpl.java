50654263fddba375ed85dea9f360c7e8a8597630
Roman <romansidorcuk1@gmail.com>
2026-07-31 19:42:44 +0300

    [user-service] Add token service and its implementation

---
+++ userservice/src/main/java/com/innowise/userservice/service/impl/TokenServiceImpl.java
@@ -0,0 +1,34 @@
+package com.innowise.userservice.service.impl;
+
+import com.innowise.userservice.service.TokenService;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TokenServiceImpl implements TokenService {
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+    public Claims getClaimsFromToken(String token) {
+        return Jwts.parser()
+                .setSigningKey(secret)
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    public Long getUserId(String token) {
+        return getClaimsFromToken(token).get("userId", Long.class);
+    }
+
+    public String getRole(String token) {
+        return getClaimsFromToken(token).get("role", String.class);
+    }
+
+    public String getTokenType(String token) {
+        return getClaimsFromToken(token).get("tokenType", String.class);
+    }
+
+}
