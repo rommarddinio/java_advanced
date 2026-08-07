51bd6b3c08427b675f3e0cec1a5da3fce2f448b0
Roman <romansidorcuk1@gmail.com>
2026-08-07 14:07:13 +0300

    [user-service] Fix TokenFilter

--- userservice/src/main/java/com/innowise/userservice/filter/TokenFilter.java
+++ userservice/src/main/java/com/innowise/userservice/filter/TokenFilter.java
@@ -11,7 +11,6 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -32,22 +31,10 @@ public class TokenFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
 
-        if (SecurityContextHolder.getContext().getAuthentication() != null) {
-            filterChain.doFilter(request, response);
-            return;
-        }
-
-        if (request.getMethod().equals("POST") && request.getRequestURI().equals("/users")) {
-            filterChain.doFilter(request, response);
-            return;
-        }
-
         String header = request.getHeader("Authorization");
 
-
         if (header == null || !header.startsWith("Bearer ")) {
-            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-            response.getWriter().write("Authorization header missing or invalid");
+            filterChain.doFilter(request, response);
             return;
         }
 
