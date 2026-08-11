34e4e389e00b368f85a054d648e8a45428517df2
Roman <romansidorcuk1@gmail.com>
2026-08-12 00:40:39 +0300

    Change toEntity method in OrderMapper

--- orderservice/src/main/java/com/innowise/orderservice/mapper/OrderMapper.java
+++ orderservice/src/main/java/com/innowise/orderservice/mapper/OrderMapper.java
@@ -1,7 +1,7 @@
 package com.innowise.orderservice.mapper;
 
+import com.innowise.orderservice.dto.order.CreateOrderDto;
 import com.innowise.orderservice.dto.order.ResponseOrderDto;
-import com.innowise.orderservice.dto.orderitem.CreateOrderItemDto;
 import com.innowise.orderservice.entity.Order;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
@@ -9,7 +9,7 @@ import org.mapstruct.Mapping;
 @Mapper(componentModel = "spring", uses = OrderItemMapper.class)
 public interface OrderMapper {
 
-    Order toEntity(CreateOrderItemDto dto);
+    Order toEntity(CreateOrderDto dto);
 
     @Mapping(source = "orderItemList", target = "items")
     ResponseOrderDto toDto(Order order);
