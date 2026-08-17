112f7da04d81694c61718ade3c71b9589046d722
Roman <romansidorcuk1@gmail.com>
2026-08-17 15:15:52 +0300

    Set nullable=false on necessary fields

--- orderservice/src/main/java/com/innowise/orderservice/entity/Item.java
+++ orderservice/src/main/java/com/innowise/orderservice/entity/Item.java
@@ -22,9 +22,10 @@ public class Item extends Auditable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(unique = true)
+    @Column(unique = true, nullable = false)
     private String name;
 
+    @Column(nullable = false)
     private BigDecimal price;
 
     private Boolean deleted = false;
--- orderservice/src/main/java/com/innowise/orderservice/entity/Order.java
+++ orderservice/src/main/java/com/innowise/orderservice/entity/Order.java
@@ -26,13 +26,14 @@ public class Order extends Auditable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "user_id")
+    @Column(name = "user_id", nullable = false)
     private Long userId;
 
     @Enumerated(value = EnumType.STRING)
+    @Column(nullable = false)
     private Status status;
 
-    @Column(name = "total_price")
+    @Column(name = "total_price", nullable = false)
     private BigDecimal totalPrice;
 
     private Boolean deleted = false;
