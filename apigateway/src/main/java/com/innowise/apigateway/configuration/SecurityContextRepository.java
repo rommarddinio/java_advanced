a207591b28387b83daf3d48bb4044f95b36c8296
Roman <romansidorcuk1@gmail.com>
2026-08-19 17:14:29 +0300

    Add configuration

---
+++ apigateway/src/main/java/com/innowise/apigateway/configuration/SecurityContextRepository.java
@@ -0,0 +1,40 @@
+package com.innowise.apigateway.configuration;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextImpl;
+import org.springframework.security.web.server.context.ServerSecurityContextRepository;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+@RequiredArgsConstructor
+public class SecurityContextRepository implements ServerSecurityContextRepository {
+
+    private final AuthenticationManager authenticationManager;
+
+    @Override
+    public Mono<SecurityContext> load(ServerWebExchange exchange) {
+        String authHeader = exchange.getRequest()
+                .getHeaders()
+                .getFirst("Authorization");
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+            String authToken = authHeader.substring(7);
+
+            UsernamePasswordAuthenticationToken auth
+                    = new UsernamePasswordAuthenticationToken(authToken, authToken);
+            return authenticationManager
+                    .authenticate(auth)
+                    .map(SecurityContextImpl::new);
+        }
+
+        return Mono.empty();
+    }
+
+    @Override
+    public Mono<Void> save(ServerWebExchange exchange, SecurityContext context) {
+        return Mono.empty();
+    }
+}
